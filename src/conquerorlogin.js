c70c0f543597ba89eeeb0434663703c07bf99b40
adewaley-a <abdullahiadewale3@gmail.com>
2026-02-17 13:20:56 +0100

    latest ESlint

--- src/conquerorlogin.js
+++ src/conquerorlogin.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import { auth, provider } from './firebase';
-import { signInWithPopup, signInWithRedirect } from 'firebase/auth'; // Switched to Popup for easier navigation
-import { useNavigate } from 'react-router-dom'; // Import the hook
+import { signInWithPopup } from 'firebase/auth'; 
+import { useNavigate } from 'react-router-dom'; 
 import './conquerorlogin.css';
 import googlogo from './googlogo.png';
 
 function Login() {
-  const navigate = useNavigate(); // Initialize the navigate function
+  const navigate = useNavigate(); 
 
   const signIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         console.log("User signed in:", result.user);
         
-        // This is the magic line that sends them to the second page
+        // Navigation to the second page
         navigate('/second-page'); 
       })
       .catch((error) => {
         alert(error.message);
       });
   };
-      
 
   return (
     <div className="login-container">
--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
-import { VisibilityProvider } from './VisibilityContext';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from 'react-router-dom';
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"></link>
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    
-    <App />
- 
-
+        <App />
   </React.StrictMode>
 );
 
