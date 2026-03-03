a76f545c182abba1f32513e2dc3bf5ea74481c23
adewaley-a <abdullahiadewale3@gmail.com>
2026-03-03 12:28:42 +0100

    game 19

--- src/GamePage.js
+++ src/GamePage.js
@@ -1,9 +1,21 @@
-import React, { useEffect, useRef, useState } from "react";
-import { io } from "socket.io-client";
+import React from "react";
 
-
-    return (
-        <div style={{ background: '#000', height: '100vh', display: 'flex', justifyContent: 'center' }}>
-            Gamepage
-        </div>
-    );
+// The function wrapper MUST start here
+export default function GamePage() {
+  
+  // The return MUST be inside the function's { } braces
+  return (
+    <div style={{ 
+      background: "#000", 
+      height: "100vh", 
+      color: "white", 
+      display: "flex", 
+      alignItems: "center", 
+      justifyContent: "center",
+      fontSize: "2rem",
+      fontFamily: "sans-serif"
+    }}>
+      GAMEPAGE TEST (CLEAN)
+    </div>
+  );
+} // The function wrapper MUST end here
